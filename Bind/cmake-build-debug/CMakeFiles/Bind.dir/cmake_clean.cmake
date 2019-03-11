6f32a36f7880d8d6d15086b9afca4e49230260a6
Artem Abramov <atem199700@gmail.com>
2019-03-11 16:34:52 +0300

    init bind

---
+++ Bind/cmake-build-debug/CMakeFiles/Bind.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Bind.dir/main.cpp.obj"
+  "Bind.pdb"
+  "Bind.exe"
+  "Bind.exe.manifest"
+  "libBind.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Bind.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
