44dee05b3ee4be4da4ebfa5b0b00f8a708f4f906
Artem Abramov <atem199700@gmail.com>
2019-03-12 16:41:12 +0300

    ehehehe

---
+++ Bind/bind.h
@@ -0,0 +1,237 @@
+#pragma once
+
+#include <iostream>
+#include <tuple>
+
+template<typename T, T ... values>
+struct integer_sequence {
+};
+
+template<typename T, typename Seq, T arg>
+struct append;
+
+template<typename T, T ... values, T arg>
+struct append<T, integer_sequence<T, values...>, arg> {
+    typedef integer_sequence<T, values..., arg> type;
+};
+
+template<typename T, T N, typename Q = void>
+struct make_integer_sequence {
+    typedef typename append<T, typename make_integer_sequence<T, N - 1>::type, N - 1>::type type;
+};
+
+template<typename T, T N>
+struct make_integer_sequence<T, N, typename std::enable_if<N == 0>::type> {
+    typedef integer_sequence<T> type;
+};
+
+template<int N>
+struct placeholder {
+};
+
+template<typename F, typename ... As>
+struct bind_t;
+
+constexpr placeholder<1> _1;
+constexpr placeholder<2> _2;
+constexpr placeholder<3> _3;
+
+template<typename T>
+struct wrapper {
+    typedef T type;
+};
+
+template<int N>
+struct wrapper<placeholder<N> &> {
+    typedef placeholder<N> type;
+};
+
+template<int N>
+struct wrapper<const placeholder<N> &> {
+    typedef placeholder<N> type;
+};
+
+template<int N>
+struct wrapper<placeholder<N> &&> {
+    typedef placeholder<N> type;
+};
+
+template<typename F, typename ... As>
+struct wrapper<bind_t<F, As...> &> {
+    typedef bind_t<F, As...> type;
+};
+
+template<typename F, typename ... As>
+struct wrapper<const bind_t<F, As...> &> {
+    typedef bind_t<F, As...> type;
+};
+
+template<typename F, typename ... As>
+struct wrapper<bind_t<F, As...> &&> {
+    typedef bind_t<F, As...> type;
+};
+
+template<typename T>
+using wrapper_t = typename wrapper<T>::type;
+
+
+
+template<typename T, typename Q>
+struct merge;
+
+template<int ... h, int ... t>
+struct merge<integer_sequence<int, h...>, integer_sequence<int, t...> > {
+    typedef integer_sequence<int, h..., t...> value;
+};
+
+template<typename ... Types>
+struct indexes;
+
+template<typename T>
+struct index {
+    typedef integer_sequence<int> value;
+};
+
+template<int N>
+struct index<placeholder<N> > {
+    typedef integer_sequence<int, N> value;
+};
+
+template<typename F, typename ... As>
+struct index<bind_t<F, As...> > {
+    typedef typename indexes<std::decay_t<As>...>::value value;
+};
+
+template<>
+struct indexes<> {
+    typedef integer_sequence<int> value;
+};
+
+template<typename H, typename ... T>
+struct indexes<H, T...> {
+    typedef typename merge<typename index<H>::value, typename indexes<T...>::value>::value value;
+};
+
+template<typename  ... T>
+using indexes_t = typename indexes<std::decay_t<T>...>::value;
+
+
+
+template<int Ind, typename S>
+struct checker;
+
+template<int Ind, int h, int ... t>
+struct checker<Ind, integer_sequence<int, h, t ...> > {
+    static constexpr int value = static_cast<int>(Ind == h) + checker<Ind, integer_sequence<int, t ...> >::value;
+};
+
+template<int Ind>
+struct checker<Ind, integer_sequence<int> > {
+    static const int value = 0;
+};
+
+template<int Ind, typename Seq>
+constexpr bool checker_t = checker<Ind, Seq>::value == 1;
+
+template<typename T, bool uniq>
+struct convert {
+    typedef T &type;
+};
+
+template<typename T>
+struct convert<T, true> {
+    typedef T &&type;
+};
+
+template<typename T, bool uniq>
+using convert_t = typename convert<T, uniq>::type;
+
+
+
+template<typename A>
+struct G {
+
+    template<typename Ar>
+    G(Ar &&a) : a(std::forward<Ar>(a)) {}
+
+    template<typename ... Bs>
+    A operator()(Bs &&...) {
+        return static_cast<A>(a);
+    }
+
+    std::decay_t<A> a;
+};
+
+template<>
+struct G<placeholder<1> > {
+
+    G(placeholder<1>) {}
+
+    template<typename B1, typename ... Bs>
+    decltype(auto) operator()(B1 &&b1, Bs &&...) {
+        return std::forward<B1>(b1);
+    }
+};
+
+template<int N>
+struct G<placeholder<N> > {
+
+    G(placeholder<N>) {}
+
+    template<typename B, typename ... Bs>
+    decltype(auto) operator()(B &&, Bs &&... bs) {
+        G<placeholder<N - 1> > next{placeholder<N - 1>()};
+        return next(std::forward<Bs>(bs)...);
+    }
+};
+
+template<typename F, typename ... As>
+struct G<bind_t<F, As...> > {
+
+    G(const bind_t<F, As...> &fun1) : fun(fun1) {};
+
+    G(bind_t<F, As...> &&fun1) : fun(std::move(fun1)) {}
+
+    template<typename ... Bs>
+    decltype(auto) operator()(Bs &&... bs) {
+        return fun(std::forward<Bs>(bs)...);
+    }
+
+private:
+    bind_t<F, As...> fun;
+};
+
+template<typename F, typename ... As>
+struct bind_t {
+
+    template<typename Fr, typename ... Asr>
+    bind_t(Fr &&f1, Asr &&... as)
+            : f(std::forward<Fr>(f1)), gs(std::forward<Asr>(as)...) {}
+
+    template<typename ... Bs>
+    decltype(auto) operator()(Bs &&... bs) {
+        return call(typename make_integer_sequence<int, sizeof...(As)>::type(),
+                    typename make_integer_sequence<int, sizeof...(Bs)>::type(), std::forward<Bs>(bs)...);
+    }
+
+private:
+    template<int ... ks, int ... kss, typename ... Bs>
+    decltype(auto) call(integer_sequence<int, ks ...>, integer_sequence<int, kss ...>, Bs &&... bs) {
+        return f(std::get<ks>(gs)(
+                static_cast<convert_t<Bs, checker_t<(kss + 1), indexes_t<As...> > > >(bs)...)...);
+    }
+
+private:
+    F f;
+    std::tuple<G<wrapper_t<As> >...> gs;
+};
+
+template<typename F, typename ... As>
+decltype(auto) bind(F &&f, As &&... as) {
+    return bind_t<std::decay_t<F>, wrapper_t<std::decay_t<As> &>...>(std::forward<F>(f), std::forward<As>(as)...);
+}
+
+template<typename F, typename ... As>
+decltype(auto) call_once_bind(F &&f, As &&... as) {
+    return bind_t<std::decay_t<F>, wrapper_t<std::decay_t<As> &&>...>(std::forward<F>(f), std::forward<As>(as)...);
+}
--- Bind/main.cpp
+++ Bind/main.cpp
@@ -1,152 +1,51 @@
-#include <cstdlib>
 #include <iostream>
-#include <functional>
+#include <string>
+#include <iostream>
+#include <utility>
 #include <tuple>
+#include "bind.h"
+using std::string;
+using std::cin;
+using std::cout;
 
-namespace bind_test
-{
-
-
-    // Stores a tuple of indices.  Used by tuple and pair, and by bind() to
-    // extract the elements in a tuple.
-    template<size_t... _Indexes>
-    struct _Index_tuple
-    {
-        constexpr _Index_tuple()
-        {}
-
-        typedef _Index_tuple<_Indexes..., sizeof...(_Indexes)> __next;
-    };
-
-    // Builds an _Index_tuple<0, 1, 2, ..., _Num-1>.
-    template<size_t _Num>
-    struct _Build_index_tuple
-    {
-        typedef typename _Build_index_tuple<_Num - 1>::__type::__next __type;
-    };
-
-    template<>
-    struct _Build_index_tuple<0>
-    {
-        typedef _Index_tuple<> __type;
-    };
-
-    template <size_t N>
-    struct arg
-    {
-        constexpr arg()
-        {}
-
-        template <typename A0, typename ... Args>
-        auto operator()(A0 const&, Args const& ... args) const
-        {
-            constexpr const arg<N - 1> next;
-            return next(args...);
-        }
-    };
-
-    template <>
-    struct arg<0>
-    {
-        constexpr arg()
-        {}
-
-        template <typename A0, typename ... Args>
-        A0 operator()(A0 const& a0, Args const& ...) const
-        {
-            return a0;
-        }
-    };
-
-    template <typename T>
-    struct const_
-    {
-        const_(T val)
-                : val(std::move(val))
-        {}
-
-        template <typename ... Args>
-        T operator()(Args const& ...) const
-        {
-            return val;
-        }
-
-    private:
-        T val;
-    };
-
-    template <typename P>
-    struct holder
-    {
-        typedef const_<P> type;
-    };
-
-    template <size_t N>
-    struct holder<arg<N>>
-    {
-        typedef arg<N> type;
-    };
-
-    template <typename F, typename ... P>
-    struct bind_t;
-
-    template <typename F, typename ... P>
-    struct holder<bind_t<F, P...>>
-    {
-        typedef bind_t<F, P...> type;
-    };
-
-    template <typename F, typename ... P>
-    struct bind_t
-    {
-        bind_t(F func, P&& ... p)
-                : func(std::move(func))
-                , p(std::forward<P>(p)...)
-        {}
-
-        template <typename ... Args>
-        auto operator()(Args const& ... args) const
-        {
-            constexpr typename _Build_index_tuple<sizeof...(P)>::__type indices;
-            return call(indices, args...);
-        }
-
-    private:
-        template <typename ... Args, size_t ... Indices>
-        auto call(_Index_tuple<Indices...>, Args const& ... args) const
-        {
-            return func(std::get<Indices>(p)(args...)...);
-        }
-
-    private:
-        F func;
-        std::tuple<typename holder<P>::type...> p;
-    };
-
-    template <typename F, typename P0, typename P1>
-    bind_t<F, P0, P1> bind(F func, P0 p0, P1 p1)
-    {
-        return bind_t<F, P0, P1>(std::move(func), std::move(p0), std::move(p1));
-    }
-
-    constexpr arg<0> _1;
-    constexpr arg<1> _2;
+void f(string a, string b, string c, string& d) {
+    cout << a << " " << b << " " << c << " " << d << "\n";
+    //d = "AZAZAA";
 }
 
-int sum(int a, int b)
-{
-    return a + b;
+void ff(int a, int b, int c, int d) {
+    cout << a << " " << b << " " << c << " " << d << "\n";
+    //d = "AZAZAA";
 }
+
+string concat(string x, string y) {
+    return x + y;
+}
+
+int add(int x, int y) {
+    return x + y;
+}
+
+struct  nonc
+{
+    nonc(const nonc&) = delete;
+    nonc(nonc&&) = default;
+    nonc() {}
+};
 
 int main()
 {
-    using namespace bind_test;
+    string s = "lol";
+    string a = "kek";
+    string b = "wow";
+    auto w = bind(f, "YOU", placeholder<2>(), bind(concat, string("TROLL+"), _3), _3);
+    w(std::move(a), std::move(b), s);
+    w("a", "b", std::move(s));
+    cout << a << "!" << b << "\n";
 
-    std::cout << (bind(&sum, 5, -5))() << std::endl;
-    std::cout << (bind(&sum, _1, 5))(-5) << std::endl;
-    std::cout << (bind(&sum, _1, _1))(0) << std::endl;
-    std::cout << (bind(&sum, _2, _2))(10, 0) << std::endl;
-    std::cout << (bind(&sum, _2, _1))(5, -5) << std::endl;
-    std::cout << (bind(&sum, bind(&sum, _2, 10), _1))(5, -15) << std::endl;
+    auto ww = bind([](nonc const&, nonc const&) {}, nonc{}, _1);
+    ww(nonc{});
+//    auto w = bind(add, _1, _2);
+//    auto ww = bind(add, w, _1);
     return 0;
 }
